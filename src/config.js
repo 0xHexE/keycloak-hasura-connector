73bc4f51c2b1f90aaa4c83a6129e9447af0eeb06
Omkar Yadav <httpsomkar@gmail.com>
2019-02-13 14:45:25 +0530

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const convict = require('convict');
+
+const config = convict({
+    env: {
+        doc: 'The application environment.',
+        format: ['production', 'development', 'test'],
+        default: 'development',
+        env: 'NODE_ENV'
+    },
+    port: {
+        doc: 'The port to bind.',
+        format: 'port',
+        default: 3000,
+        env: 'PORT',
+        arg: 'port'
+    },
+    kcConfig: {
+        clientId: {
+            doc: 'Keycloak config',
+            format: String,
+            default: null,
+            env: 'KEYCLOAK_CLIENT_ID',
+        },
+        bearerOnly: {
+            doc: 'keycloak auth type',
+            format: Boolean,
+            default: true,
+            env: 'KEYCLOAK_BEARER_ONLY',
+        },
+        serverUrl: {
+            doc: 'Keycloak server url',
+            format: String,
+            default: 'https://accounts.f4erp.com/auth',
+            env: 'KEYCLOAK_SERVER_URL',
+        },
+        realm: {
+            doc: 'realm name keycloak',
+            format: String,
+            default: null,
+            env: 'KEYCLOAK_REALM',
+        },
+        credentials: {
+            secret: {
+                doc: 'KEYCLOAK_SECRET',
+                format: String,
+                sensitive: true,
+                default: null,
+                env: 'KEYCLOAK_SECRET',
+            },
+        },
+    },
+});
+
+const env = config.get('env');
+const configPath = './config/' + env + '.json';
+
+if (fs.existsSync(configPath)) {
+    config.loadFile(configPath);
+}
+
+config.validate({allowed: 'strict'});
+
+module.exports = config;
---
+++ src/main.js
@@ -0,0 +1,31 @@
+const app = require('express')();
+const Keycloak = require('keycloak-connect');
+const config = require('./config');
+const keycloak = new Keycloak({  }, config.get('kcConfig'));
+
+app.get('/', keycloak.middleware(), (res, req) => {
+    if (!res.kauth.grant) {
+        return req.sendStatus(401);
+    }
+
+    const tokenContent = res.kauth.grant.access_token.content;
+
+    const organizationId = tokenContent.group
+        .sort((v1, v2) => v1.split('/').length - v2.split('/').length)[0].split('/')[1];
+    const subGroups = tokenContent.group.map(res => res.replace(`/${ organizationId }`, '')).join(',');
+
+    const hasuraVariables = {
+        'X-Hasura-Role': (tokenContent.resource_access[config.get('kcConfig.clientId')] || {roles: []}).roles.join(','),
+        'X-Hasura-Realm-Role': tokenContent.realm_access.roles.join(','),
+        'X-Hasura-User-Id': tokenContent.id,
+        'X-Hasura-Organization-Id': organizationId,
+        'X-Hasura-Sub-Groups-Id': subGroups,
+    };
+
+    req.status(200)
+        .jsonp(hasuraVariables);
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000 port');
+});
