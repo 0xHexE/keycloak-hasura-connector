ff6b68e5bb6b07c4862ca729c9df9d8df599160a
Omkar Yadav <httpsomkar@gmail.com>
2019-02-14 21:52:56 +0530

    Updated the version of hasura

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ app.get('/', keycloak.middleware(), (res, req) => {
 
     const organizationId = tokenContent.group
         .sort((v1, v2) => v1.split('/').length - v2.split('/').length)[0].split('/')[1];
+
     const subGroups = tokenContent.group.map(res => res.replace(`/${ organizationId }`, '')).join(',');
 
     const hasuraVariables = {
